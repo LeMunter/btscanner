f35ea9dd06aadea9273b9ed9c6b316ef422b68fe
lemunter <antonmunter@gmail.com>
2022-09-01 22:03:41 +0200

    add some bad stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { FaBluetooth } from 'react-icons/fa'
 import './firebase'
 import useAnalyticsEventTracker from 'hooks/useAnalyticsEventTracker'
+import { ToastContainer, toast, cssTransition, ToastOptions } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import 'animate.css/animate.min.css'
+
+const bounce = cssTransition({
+  enter: 'animate__animated animate__bounceIn',
+  exit: 'animate__animated animate__bounceOut',
+})
 
 interface IAppProps {}
 
@@ -14,38 +22,94 @@ const App: React.FC<IAppProps> = (props) => {
   const [btDevice, setBtDevice] = useState<BluetoothDevice | null>(null)
   const gaEventTracker = useAnalyticsEventTracker('Home')
 
-  // useEffect(() => {
-  //   console.log(perf.dataCollectionEnabled)
-  // }, [])
-
-  const scanDevices = async () => {
-    gaEventTracker('Scan event')
-    const foundBtDevice = await navigator.bluetooth
-      .requestDevice(btSettings)
-      .catch((error: Error) => {
-        console.log(error.message)
-      })
-
-    if (foundBtDevice) {
-      setBtDevice(foundBtDevice)
+  const notify = (msg: string, { error = false }) => {
+    const options: ToastOptions = {
+      pauseOnFocusLoss: false,
+      autoClose: 1000,
+      theme: 'colored',
+      transition: bounce,
     }
+
+    if (error) {
+      toast.error(msg, options)
+    } else {
+      toast.success(msg, options)
+    }
+  }
+
+  const onScanButtonClick = async () => {
+    if (!isBluetoothEnabled()) {
+      gaEventTracker('No bluetooth')
+      notify("Browser doesn't support bluetooth", { error: true })
+      return
+    }
+
+    try {
+      const foundBtDevice = await navigator.bluetooth
+        .requestDevice(btSettings)
+        .catch((error: Error) => {
+          // console.log(error.message)
+        })
+
+      if (foundBtDevice) {
+        setBtDevice(foundBtDevice)
+      }
+    } catch (error) {
+      notify('Something went wrong :(', { error: true })
+    }
+  }
+
+  const isBluetoothEnabled = () => {
+    if (!navigator.bluetooth) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const onDisconnectButtonClick = () => {
+    if (!btDevice) return
+    if (!btDevice.gatt) return
+    setBtDevice(null)
   }
 
   return (
     <div className='cool-bg h-screen w-screen grid place-items-center'>
-      <div className='w-32 h-32 border-8 rounded-full animate-pulse opacity-40 absolute animate-ping' />
-      <div className='w-44 h-44 border-8 rounded-full animate-pulse opacity-20 absolute animate-ping' />
-      <div className='w-56 h-56 border-8 rounded-full animate-pulse opacity-20 absolute animate-ping' />
+      <div className='grid place-items-center'>
+        {btDevice ? (
+          <>
+            <div>{btDevice.name}</div>
+            <button
+              className='relative bg-gradient-to-tr from-red-600 via-pink-700 to-red-700 hover:opacity-80 text-white font-bold py-2 px-4 rounded inline-flex items-center gap-5'
+              onClick={onDisconnectButtonClick}
+            >
+              <span>
+                <FaBluetooth size={35} />
+              </span>
+              <span>Disconnect</span>
+            </button>
+          </>
+        ) : (
+          <>
+            <div className='w-32 h-32 border-8 rounded-full absolute animate-pulse opacity-40 animate-ping' />
+            <div className='w-44 h-44 border-8 rounded-full absolute animate-pulse opacity-20 animate-ping' />
+            <div className='w-56 h-56 border-8 rounded-full absolute animate-pulse opacity-20 animate-ping' />
+            <button
+              className='relative bg-neutral-50 hover:opacity-80 text-slate-800 font-bold py-2 px-4 rounded inline-flex items-center gap-5'
+              onClick={onScanButtonClick}
+            >
+              <span>
+                <FaBluetooth size={35} />
+              </span>
+              <span>SCAN</span>
+            </button>
+          </>
+        )}
+      </div>
 
-      <button
-        className='relative bg-neutral-50 hover:opacity-80 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center gap-5'
-        onClick={scanDevices}
-      >
-        <span>
-          <FaBluetooth size={35} />
-        </span>
-        <span>SCAN</span>
-      </button>
+      <span className='absolute'>
+        <ToastContainer limit={3} />
+      </span>
     </div>
   )
 }
